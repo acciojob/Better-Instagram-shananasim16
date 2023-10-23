9efbf30cd086c5ba4cc2583cbfb5b4891a8df758
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-23 11:57:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+const images = document.querySelectorAll('.image');
+
+let selectedImage = null;
+
+images.forEach((image) => {
+  image.addEventListener('dragstart', () => {
+    selectedImage = image;
+    image.classList.add('selected');
+  });
+
+  image.addEventListener('dragend', () => {
+    selectedImage = null;
+    image.classList.remove('selected');
+  });
+
+  image.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  image.addEventListener('drop', () => {
+    if (selectedImage !== image) {
+      const selectedImageUrl = selectedImage.style.backgroundImage;
+      const imageUrl = image.style.backgroundImage;
+
+      selectedImage.style.backgroundImage = imageUrl;
+      image.style.backgroundImage = selectedImageUrl;
+    }
+  });
+});
